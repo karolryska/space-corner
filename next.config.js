0f6e835b8484c34400504d5eb11a75f3e6bad5fa
Karol Ryska <karolryska@gmail.com>
2022-07-27 00:14:17 +0200

    chore: initial config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
+const path = require('path');
 
-module.exports = nextConfig
+const nextConfig = {
+    sassOptions: {
+        includePaths: [path.join(__dirname, 'src/styles')],
+    },
+    reactStrictMode: true,
+    swcMinify: true,
+};
+
+module.exports = nextConfig;
