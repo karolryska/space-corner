4645e8e30aef338ed97ff8fc4a3cf642eabca6f1
Karol Ryska <karolryska@gmail.com>
2022-07-31 23:36:38 +0200

    chore: install svgr

--- next.config.js
+++ next.config.js
@@ -5,6 +5,15 @@ const nextConfig = {
     sassOptions: {
         includePaths: [path.join(__dirname, 'src/styles')],
     },
+    webpack(config) {
+        config.module.rules.push({
+            test: /\.svg$/i,
+            issuer: /\.[jt]sx?$/,
+            use: ['@svgr/webpack'],
+        });
+
+        return config;
+    },
     reactStrictMode: true,
     swcMinify: true,
 };
