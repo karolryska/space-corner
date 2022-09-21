66a5119b8062bf823e5347ccfced579a30e538c7
Karol Ryska <karolryska@gmail.com>
2022-09-22 00:40:15 +0200

    feat: initialize section component

---
+++ src/components/sectionCarousel/SectionCarousel.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import classNames from 'classnames/bind';
+import styles from './sectionCarousel.module.scss';
+
+const cn = classNames.bind(styles);
+
+interface Props {
+    children: React.ReactNode;
+    title: string;
+}
+
+const SectionCarousel = ({ children, title }: Props) => {
+    return (
+        <section className={cn('wrapper')}>
+            <span className={cn('innerWrapper')}>
+                <h2 className={cn('title')}>{title}</h2>
+                <span className={cn('body')}>{children}</span>
+            </span>
+            <div className={cn('carousell')}></div>
+        </section>
+    );
+};
+
+export default SectionCarousel;
