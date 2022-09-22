cf81d37b163a8be87b194ae3a23673c0108a8fcb
Karol Ryska <karolryska@gmail.com>
2022-09-22 22:08:48 +0200

    wip

--- src/components/sectionCarousel/SectionCarousel.tsx
+++ src/components/sectionCarousel/SectionCarousel.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import classNames from 'classnames/bind';
+import OfferThumbnail from 'components/offerThumbnail/OfferThumbnail';
 import styles from './sectionCarousel.module.scss';
 
 const cn = classNames.bind(styles);
@@ -16,7 +17,13 @@ const SectionCarousel = ({ children, title }: Props) => {
                 <h2 className={cn('title')}>{title}</h2>
                 <span className={cn('body')}>{children}</span>
             </span>
-            <div className={cn('carousel')}></div>
+            <div className={cn('carousel')}>
+                <OfferThumbnail />
+                <OfferThumbnail />
+                <OfferThumbnail />
+                <OfferThumbnail />
+                <OfferThumbnail />
+            </div>
         </section>
     );
 };
