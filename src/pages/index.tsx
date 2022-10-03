404326dca563231f39d2df14c836a255c0f3e3dc
Karol Ryska <karolryska@gmail.com>
2022-10-03 22:50:14 +0200

    feat: initialie home page component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,42 @@
+import Link from 'next/link';
 import type { NextPage } from 'next';
+import Navigation from 'components/navigation/Navigation';
+import Hero from 'components/hero/Hero';
+import Section from 'components/section/Section';
 
-const Home: NextPage = () => {
-    return <main>Hello world</main>;
+const HomePage: NextPage = () => {
+    return (
+        <>
+            <Navigation />
+            <Hero title={['Szukasz idealnego', ' miejsca do pracy?', 'Mamy takie.']}></Hero>
+            <Section title="pomysł">
+                <span>
+                    Czasy wszechobecnej pandemii nie są łatwe. Praca z domu nie dla każdego jest
+                    spełnieniem marzeń, nie każdy też może sobie na nią pozwolić.
+                </span>
+                <span>
+                    Zajmujemy się wynajmem przestrzeni do pracy, w której poczujesz się komfortowo i
+                    nie będziesz z utęsknieniem wyczekiwał aż zegarek wybije godzinę 17.
+                </span>
+                <span>Sprawdź, co nasze biura mają Ci do zoferowania.</span>
+                <Link href="/">Dowiedz się więcej.</Link>
+            </Section>
+
+            <Section title="pomysł" reverse>
+                <span>
+                    Czy wasze biura są pieskolubne? Czy są otwarte 24/7? Czy można wynająć biurko
+                    tylko na jeden dzień? Czy jest miejsce do przeprowadzenia wideokonferencji?
+                </span>
+                <span>Cztery razy TAK.</span>
+                <span>
+                    To tylko kilka z pytań, które dostajemy regularnie. Jeśli chcesz dowiedzieć się
+                    więcej lub dopytać o szczególny, możesz skontaktować się z nami mailowo lub
+                    telefonicznie.
+                </span>
+                <Link href="/">Skontaktuj się z nami.</Link>
+            </Section>
+        </>
+    );
 };
 
-export default Home;
+export default HomePage;
